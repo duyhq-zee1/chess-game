27720ce07254721467768678f95c0120f5d6ed6c
Quang Minh <onelove4everu@gmail.com>
2024-08-15 18:33:39 +0700

    refactor abstract class based on new class diagram

--- chessGame.js
+++ chessGame.js
@@ -1,164 +1,185 @@
+class ChessPieceBase {
+    constructor(white) {
+        if (this.constructor === ChessPieceBase) {
+            throw new Error("Cannot instantiate abstract class ChessPieceBase.");
+        }
 
+        // Validate that 'white' is a boolean
+        if (typeof white !== 'boolean') {
+            throw new Error("The 'white' parameter must be a boolean value.");
+        }
+
+        this.white = white;
+        this.killed = false;
+    }
+
+    canMove(board, startCell, endCell) {
+        // Abstract method; should be implemented in subclass
+    }
+}
+
+class King extends ChessPieceBase {
+    constructor(white) {
+        super(white);
+        this.castlingDone = false;
+    }
+
+    canMove(board, startCell, endCell) {
+        // Implement King's movement logic here
+    }
+}
+
+class Queen extends ChessPieceBase {
+    constructor(white) {
+        super(white);
+    }
+
+    canMove(board, startCell, endCell) {
+        // Implement Queen's movement logic here
+    }
+}
+
+// Repeat for Bishop, Knight, Rook, and Pawn classes
+
+class Cell {
+    constructor(x, y, label) {
+        // Validate that x and y are within the bounds of an 8x8 board
+        if (x < 0 || x >= 8 || y < 0 || y >= 8) {
+            throw new Error(`Invalid coordinates (${x}, ${y}). Coordinates must be between 0 and 7.`);
+        }
+
+        // Validate that label is a string
+        if (typeof label !== 'string') {
+            throw new Error("The 'label' parameter must be a string.");
+        }
+
+        this.x = x;
+        this.y = y;
+        this.label = label;
+        this.piece = null;
+    }
+
+    getPiece() {
+        // Abstract method; should be implemented in subclass
+    }
+
+    setPiece(piece) {
+        // Abstract method; should be implemented in subclass
+    }
+}
 
 class Board {
     constructor() {
-        this.cell = [];
+        // Initialize board properties here if necessary
+        // For example, setting up an 8x8 board
     }
 
-    moveChessPiece() {
-        
+    moveChessPiece(chessPiece, destinationCell) {
+        // Abstract method; should be implemented in subclass
     }
 
     checkEndGame() {
-        
+        // Abstract method; should be implemented in subclass
     }
 
-    setUpBoard() {
-        
+    setUpBoard(chessPieces) {
+        // Abstract method; should be implemented in subclass
     }
 }
 
-class Cell {
-    constructor(row, column, chessPiece) {
-        this.row = row;
-        this.column = column;
-        this.chessPiece = chessPiece;
-    }
-
-    setChessPiece(chessPieceBase) {
-        this.chessPiece = chessPieceBase;
-    }
-
-    getChessPiece() {
-        return this.chessPiece;
-    }
-}
 class Player {
-    moveChessPiece() {
-        
+    constructor() {
+        // Initialize player properties here if necessary
+    }
+
+    moveChessPiece(chessPiece, destinationCell) {
+        // Abstract method; should be implemented in subclass
     }
 
     undoMove() {
-
+        // Abstract method; should be implemented in subclass
     }
 
     redoMove() {
-
+        // Abstract method; should be implemented in subclass
     }
 }
 
-class ChessPieceBase {
-  moveStrategies = [];
+class History {
+    constructor() {
+        if (this.constructor === History) {
+            throw new Error("Cannot instantiate abstract class History.");
+        }
+        this.moves = [];
+        this.redoMoves = [];
+    }
 
-  setPlayer = (player) => {};
+    getMove(index) {
+        // Abstract method; should be implemented in subclass
+    }
 
-  checkAvailableMoveCells = () => {};
+    addMove(chessPiece, destinationCell) {
+        // Abstract method; should be implemented in subclass
+    }
 
-  doMove = () => {};
+    showHistory() {
+        // Abstract method; should be implemented in subclass
+    }
+
+    undoMove() {
+        // Abstract method; should be implemented in subclass
+    }
+
+    redoMove() {
+        // Abstract method; should be implemented in subclass
+    }
 }
 
-class IMoveStrategy {
-  checkAvailableMoveCells = () => {};
+class HistoryMove {
+    constructor(chessPiece, destinationCell) {
+        // Validate that chessPiece is an instance of ChessPieceBase
+        if (!(chessPiece instanceof ChessPieceBase)) {
+            throw new Error("The 'chessPiece' parameter must be an instance of ChessPieceBase.");
+        }
+        // Validate that destinationCell is an instance of Cell
+        if (!(destinationCell instanceof Cell)) {
+            throw new Error("The 'destinationCell' parameter must be an instance of Cell.");
+        }
 
-  doMove = () => {};
-}
-
-class MoveStraight extends IMoveStrategy {
-  checkAvailableMoveCells = () => {};
-
-  doMove = () => {};
-}
-
-class MoveDiagonal extends IMoveStrategy {
-  checkAvailableMoveCells = () => {};
-
-  doMove = () => {};
-}
-
-class MoveL extends IMoveStrategy {
-  checkAvailableMoveCells = () => {};
-
-  doMove = () => {};
-}
-
-class MaxRangeDecorator extends IMoveStrategy {
-  constructor(move) {
-    this.move = move;
-  }
-
-  checkAvailableMoveCells = () => {};
-
-  doMove = () => {};
-}
-
-class OnlyFowardDecorator extends IMoveStrategy {
-  constructor(move) {
-    this.move = move;
-  }
-
-  checkAvailableMoveCells = () => {};
-
-  doMove = () => {};
+        this.chessPiece = chessPiece;
+        this.destinationCell = destinationCell;
+    }
 }
 
 class Game {
-  constructor(board, history, whilePlayer, blackPlayer) {
-    this.board = board;
-    this.history = history;
-    this.whilePlayer = whilePlayer;
-    this.blackPlayer = blackPlayer;
-  }
+    constructor() {
+        if (this.constructor === Game) {
+            throw new Error("Cannot instantiate abstract class Game.");
+        }
 
-  startGame = () => {
-    console.log('Start Game');
-  };
-  endGame = () => {
-    console.log('End Game');
-  };
+        this.board = new Board();
+        this.history = new History();
+        this.whitePlayer = new Player(); // Should be derived classes
+        this.blackPlayer = new Player(); // Should be derived classes
+    }
 
-  undoMove = () => {
-    console.log('Undo Move');
-  };
+    startGame() {
+        // Abstract method; should be implemented in subclass
+    }
 
-  redoMove = () => {
-    console.log('Redo Move');
-  };
+    endGame() {
+        // Abstract method; should be implemented in subclass
+    }
 
-  showHistory = () => {
-    console.log('Show history');
-  };
-}
+    undoMove() {
+        this.history.undoMove();
+    }
 
-class history {
-  constructor(board) {
-    this.moves = [];
-    this.board = board;
-  }
+    redoMove() {
+        this.history.redoMove();
+    }
 
-  undoMove = () => {
-    console.log('Undo Move');
-  };
-
-  redoMove = () => {
-    console.log('Redo Move');
-  };
-
-  showHistory = () => {
-    console.log('Show history');
-  };
-
-  getMove = () => {
-    console.log('Get Move');
-  };
-
-  addMove = () => {
-    console.log('Add move');
-  };
-}
-
-class historyMove {
-  constructor(chessPiece, cell) {
-    this.chessPiece = chessPiece;
-    this.cell = cell;
-  }
+    showHistory() {
+        this.history.showHistory();
+    }
 }
---
+++ oldchessGame.js
@@ -0,0 +1,164 @@
+
+
+class Board {
+    constructor() {
+        this.cell = [];
+    }
+
+    moveChessPiece() {
+        
+    }
+
+    checkEndGame() {
+        
+    }
+
+    setUpBoard() {
+        
+    }
+}
+
+class Cell {
+    constructor(row, column, chessPiece) {
+        this.row = row;
+        this.column = column;
+        this.chessPiece = chessPiece;
+    }
+
+    setChessPiece(chessPieceBase) {
+        this.chessPiece = chessPieceBase;
+    }
+
+    getChessPiece() {
+        return this.chessPiece;
+    }
+}
+class Player {
+    moveChessPiece() {
+        
+    }
+
+    undoMove() {
+
+    }
+
+    redoMove() {
+
+    }
+}
+
+class ChessPieceBase {
+  moveStrategies = [];
+
+  setPlayer = (player) => {};
+
+  checkAvailableMoveCells = () => {};
+
+  doMove = () => {};
+}
+
+class IMoveStrategy {
+  checkAvailableMoveCells = () => {};
+
+  doMove = () => {};
+}
+
+class MoveStraight extends IMoveStrategy {
+  checkAvailableMoveCells = () => {};
+
+  doMove = () => {};
+}
+
+class MoveDiagonal extends IMoveStrategy {
+  checkAvailableMoveCells = () => {};
+
+  doMove = () => {};
+}
+
+class MoveL extends IMoveStrategy {
+  checkAvailableMoveCells = () => {};
+
+  doMove = () => {};
+}
+
+class MaxRangeDecorator extends IMoveStrategy {
+  constructor(move) {
+    this.move = move;
+  }
+
+  checkAvailableMoveCells = () => {};
+
+  doMove = () => {};
+}
+
+class OnlyFowardDecorator extends IMoveStrategy {
+  constructor(move) {
+    this.move = move;
+  }
+
+  checkAvailableMoveCells = () => {};
+
+  doMove = () => {};
+}
+
+class Game {
+  constructor(board, history, whilePlayer, blackPlayer) {
+    this.board = board;
+    this.history = history;
+    this.whilePlayer = whilePlayer;
+    this.blackPlayer = blackPlayer;
+  }
+
+  startGame = () => {
+    console.log('Start Game');
+  };
+  endGame = () => {
+    console.log('End Game');
+  };
+
+  undoMove = () => {
+    console.log('Undo Move');
+  };
+
+  redoMove = () => {
+    console.log('Redo Move');
+  };
+
+  showHistory = () => {
+    console.log('Show history');
+  };
+}
+
+class history {
+  constructor(board) {
+    this.moves = [];
+    this.board = board;
+  }
+
+  undoMove = () => {
+    console.log('Undo Move');
+  };
+
+  redoMove = () => {
+    console.log('Redo Move');
+  };
+
+  showHistory = () => {
+    console.log('Show history');
+  };
+
+  getMove = () => {
+    console.log('Get Move');
+  };
+
+  addMove = () => {
+    console.log('Add move');
+  };
+}
+
+class historyMove {
+  constructor(chessPiece, cell) {
+    this.chessPiece = chessPiece;
+    this.cell = cell;
+  }
+}
